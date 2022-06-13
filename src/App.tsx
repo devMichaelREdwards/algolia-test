681c18794ee925ba7c018aca105dfa9f6c4ac5a6
Michael Edwards <michaelredwardsdev@gmail.com>
2022-06-13 15:09:50 -0400

    Algolia index test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import algoliasearch from "algoliasearch/lite";
+import {
+  Configure,
+  Highlight,
+  Hits,
+  InstantSearch,
+  Pagination,
+  SearchBox,
+} from "react-instantsearch-hooks-web";
+import "./App.scss";
+import "instantsearch.css/themes/satellite.css";
 
-function App() {
+const searchClient = algoliasearch(
+  "CAZAQ1YGVM",
+  "1210b4146e3ef1c2719056e673cf138b"
+);
+
+interface IHit {
+  hit: any;
+}
+
+function Hit({ hit }: IHit) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className="hit-name">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Highlight attribute="product_id" hit={hit} />{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
+}
+
+const Search = () => {
+  return (
+    <InstantSearch searchClient={searchClient} indexName="dev_mre">
+      <Configure hitsPerPage={21} />
+      <SearchBox />
+      <div className="results">
+        <Hits hitComponent={Hit} />
+      </div>
+      <Pagination />
+    </InstantSearch>
+  );
+};
+
+function App() {
+  return <Search />;
 }
 
 export default App;
